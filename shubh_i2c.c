e6147b2941b48c2affb540ba760dc320b73a935b
shubhtrix <shubham.trikha@gmail.com>
2018-11-04 20:42:16 +0300

    device checked now need to Setup the interface with device ID.

--- shubh_i2c.c
+++ shubh_i2c.c
@@ -3,7 +3,18 @@
 /* Fucking start of the program man. */
 void main() {
 
+	int rev;
+	char *device;
+
 	printf("This is going to be fun.\n");
+
+	// Check pi device.
+	rev = piGpioLayout ();
+	printf("Revison is :: %d.\n", rev);
+	if (rev == 1)
+		device = "/dev/i2c-0";
+	else 
+		device = "/dev/i2c-1";
 
 	return;
 }
